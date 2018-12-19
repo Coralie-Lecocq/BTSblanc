bb1d13e944d408ed6c6a452b850f878ea39d54db
Eunie <32666165+Eunie@users.noreply.github.com>
2018-12-19 14:20:36 +0100

    ajout du nouvel onglet pour le BO

---
+++ site_vitrine/BO.php
@@ -0,0 +1,9 @@
+<div class="banner10">
+    <div class="container">
+        <h2>Consultation du stock</h2> 
+        
+        <h2>Modification du stock</h2> 
+        
+        <h2>ajout d'un noubeau produit</h2> 
+    </div>
+</div>
